fd308c43d3652edef3da4fa12ff27394b69a2378
Mahmood Ali <mahmood@rockset.com>
2023-06-04 17:08:43 -0700

    show where topics are destroyed

--- src/rdkafka_topic.c
+++ src/rdkafka_topic.c
@@ -70,6 +70,27 @@ static void rd_kafka_topic_keep_app(rd_kafka_topic_t *rkt) {
                 rd_kafka_topic_keep(rkt);
 }
 
+#include <execinfo.h>
+
+static void print_trace(void)
+{
+  void *array[10];
+  char **strings;
+  int size, i;
+
+  size = backtrace (array, 10);
+  strings = backtrace_symbols (array, size);
+  if (strings != NULL)
+  {
+
+    fprintf (stderr, "Obtained %d stack frames.\n", size);
+    for (i = 0; i < size; i++)
+      fprintf (stderr, "%s\n", strings[i]);
+  }
+
+  free (strings);
+}
+
 /**
  * @brief drop rkt app reference
  */
@@ -80,6 +101,7 @@ static void rd_kafka_topic_destroy_app(rd_kafka_topic_t *app_rkt) {
 
         rd_kafka_topic_wrlock(rkt);
         if (unlikely(rd_refcnt_sub(&rkt->rkt_app_refcnt) == 0)) {
+                print_trace();
                 rd_bool_t destroy_partitions =
                     rkt->rkt_rk->rk_conf
                         .consumer_destroy_topic_partitions_on_cleanup &&
